3beca45a6a4376505160227ef15b00db7f06e679
Com.Lan <ComlanGiovanni@users.noreply.github.com>
2025-05-26 22:52:42 +0000

    chore: remove things in Trainning i guess i am tranning for the exam lol

---
+++ Trainning/cpp_module00/Warlock.cpp
@@ -0,0 +1,47 @@
+#include "Warlock.hpp"
+
+Warlock::Warlock(std::string const &name, std::string const &title) : name(name), title(title)
+{
+    std::cout << name << ":" << "This looks like another boring day." << std::endl;
+}
+
+Warlock::~Warlock(void)
+{
+    std::cout << name << ": My job here is done!" << std::endl;
+}
+
+std::string const &Warlock::getName(void) const
+{
+    return (this->name);
+}
+
+std::string const &Warlock::getTitle(void) const
+{
+    return (this->title);
+}
+
+void Warlock::setTitle(std::string const &title)
+{
+    this->title = title;
+}
+
+void Warlock::introduce() const
+{
+    std::cout << name << ":" <<  " I am " << name << ", " << title << "!" << std::endl;
+}
+
+int main()
+{
+  Warlock const richard("Richard", "Mistress of Magma");
+  richard.introduce();
+  std::cout << richard.getName() << " - " << richard.getTitle() << std::endl;
+
+  Warlock* jack = new Warlock("Jack", "the Long");
+  jack->introduce();
+  jack->setTitle("the Mighty");
+  jack->introduce();
+
+  delete jack;
+
+  return (0);
+}
---
+++ Trainning/cpp_module00/Warlock.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef WARLOCK_HPP
+#define WARLOCK_HPP
+
+#include <string>
+#include <iostream>
+
+class Warlock
+{
+private:
+    std::string name;
+    std::string title;
+    Warlock(void);
+    Warlock(Warlock const &rhs);
+    Warlock &operator=(Warlock const &rhs);
+
+public:
+    Warlock(std::string const &name, std::string const &title);
+    ~Warlock(void);
+    std::string const &getName(void) const;
+    std::string const &getTitle(void) const;
+    void setTitle(std::string const &title);
+    void introduce() const;
+};
+
+#endif
